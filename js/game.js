d2ad2eabc80a7cfb5c265934b2175630f8290b10
Wade7wastaken <david@thecallenders.com>
2023-02-16 09:21:27 -0600

    optimize game.js

--- js/game.js
+++ js/game.js
@@ -1,31 +1,30 @@
 const params = new URLSearchParams(window.location.search);
-const origin = sessionStorage.getItem("origin");
-const base = document.getElementById("base");
 
 switch (localStorage.emu) {
 	case "EJS": {
+		const base = document.getElementById("base");
+
+		// ejs requires an inner div element to work. idk why
 		const innerDiv = document.createElement("div");
 		innerDiv.id = "emulator";
 		base.appendChild(innerDiv);
 
-		var EJS_player = "#emulator";
+		var EJS_player = "#emulator"; // id of the inner div
 		var EJS_gameUrl = "ROMs/" + params.get("ver") + "/" + params.get("game") + ".zip";
 		var EJS_core = params.get("ver");
-		var EJS_DEBUG_XX = true;
+		var EJS_DEBUG_XX = true; // keeping debug alwasy on because its handy and doesn't hurt performance too much
 		if (localStorage.oldCores == "1") {
 			var EJS_oldCores = true;
 			console.log("Using old cores");
 		}
 
-		if (localStorage.emuWidth) {
-			base.style.width = String(localStorage.emuWidth) + "px";
-		}
-		if (localStorage.emuHeight) {
-			base.style.height = String(localStorage.emuHeight) + "px";
-		}
+		if (localStorage.emuWidth) base.style.width = String(localStorage.emuWidth) + "px";
+
+		if (localStorage.emuHeight) base.style.height = String(localStorage.emuHeight) + "px";
 
 		const emuloader = document.createElement("script");
 		if (localStorage.origin == "1") {
+			// Load from github using githack
 			var EJS_pathtodata = "https://raw.githack.com/EmulatorJS/EmulatorJS/main/data/";
 			emuloader.src = "https://raw.githack.com/EmulatorJS/EmulatorJS/main/data/loader.js";
 			console.log("Loading from github");
@@ -46,12 +45,9 @@ switch (localStorage.emu) {
 		var NepEmu = params.get("ver");
 		var NepZoom = "enable";
 
-		if (localStorage.emuWidth) {
-			NepMaxWidth = String(localStorage.emuWidth) + "px";
-		}
-		if (localStorage.emuHeight) {
-			NepMaxHeight = String(localStorage.emuHeight) + "px";
-		}
+		if (localStorage.emuWidth) NepMaxWidth = String(localStorage.emuWidth) + "px";
+		
+		if (localStorage.emuHeight) NepMaxHeight = String(localStorage.emuHeight) + "px";
 
 		const emuloader = document.createElement("script");
 		emuloader.src = "https://mem.neptunjs.com/njs/njsLoader.js";
