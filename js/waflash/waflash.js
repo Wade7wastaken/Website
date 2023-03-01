1f1c1e820bc92bd194f282422075061186d760bb
Wade7wastaken <david@thecallenders.com>
2023-03-01 08:23:59 -0600

    extract waflash js into .js files

---
+++ js/waflash/waflash.js
@@ -0,0 +1,14 @@
+var ua = navigator.userAgent.toLowerCase();
+if (
+	(navigator.appName == "Netscape" && ua.indexOf("trident") != -1) ||
+	ua.indexOf("msie") != -1
+) {
+	var waf = document.getElementById("waflashStatus");
+	waf.style.display = "block";
+	waf.innerText =
+		"??? ?????(IE) ??? ??? ???? ???? !\n??, ???? ?? ????? ??????.";
+	gtag("event", "connect_from_ie", {
+		event_category: "error",
+		event_label: ua,
+	});
+}
---
+++ js/waflash/waflashmodule.js
@@ -0,0 +1,51 @@
+let is_mobile = /Mobi/i.test(window.navigator.userAgent);
+if (is_mobile) {
+	function scrollToSubject() {
+		try {
+			window.scrollTo({ top: 100, left: 0, behavior: "smooth" });
+		} catch (e) {}
+	}
+	scrollToSubject();
+	window.addEventListener("orientationchange", function () {
+		setTimeout(scrollToSubject, 100);
+	});
+} else {
+	document.getElementById("canvas").focus();
+}
+document.getElementById("canvas").addEventListener("keydown", function (ev) {
+	ev.preventDefault();
+	ev.stopPropagation();
+});
+document.getElementById("canvas").addEventListener("click", function () {
+	document.getElementById("canvas").focus();
+});
+document.addEventListener(
+	"mousedown",
+	(function () {
+		const canvasElement = document.getElementById("canvas");
+		let focused = false;
+		return function (ev) {
+			if (ev.target == canvasElement) {
+				if (!focused) {
+					canvasElement.focus();
+					focused = true;
+				}
+			} else {
+				if (focused) {
+					focused = false;
+				}
+			}
+			return true;
+		};
+	})(),
+);
+import { createWaflash } from "https://cdn.jsdelivr.net/gh/AndreajnRcm4/b398dl2h74v@9a23fbb2373d743a564873251cbb57736ebc1f73/js/waflash-player.min.js";
+
+const params = new URLSearchParams(window.location.search);
+
+createWaflash(
+	"https://rawcdn.githack.com/wade7wastaken/TrulyUnblockedGamesSWF/main/" +
+		params.get("game") +
+		".swf",
+	window.wafOptions || {},
+);
