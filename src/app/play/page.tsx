c7409ab8346a991e41f5ece4908c27e053d1df0d
David C <david@thecallenders.com>
2023-10-27 15:09:28 -0500

    suspense boundary

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1,9 +1,13 @@
-import { type FC } from "react";
+import { Suspense, type FC } from "react";
 
 import { PlayPage } from "@components/PlayPage";
 
 const Home: FC = () => {
-  return <PlayPage />;
+  return (
+    <Suspense>
+      <PlayPage />
+    </Suspense>
+  );
 };
 
 export default Home;
