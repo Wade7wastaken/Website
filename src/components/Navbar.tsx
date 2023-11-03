3ab5f4d771b81cbceb10db79cdfd6680a107094c
David C <david@thecallenders.com>
2023-11-03 18:22:57 -0500

    update links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,10 +7,10 @@ export const Navbar: FC = () => {
   return (
     <div className="mt-16">
       <div className="bg-gray-700 fixed bottom-0 left-0 z-50 w-full p-1 flex gap-8 justify-center">
-        <Link href="https://github.com/Wade7wastaken/TrulyUnblockedGames">
+        <Link href="https://github.com/Wade7wastaken/Website">
           GitHub
         </Link>
-        <Link href="https://github.com/Wade7wastaken/TrulyUnblockedGames/issues/new">
+        <Link href="https://github.com/Wade7wastaken/Website/issues/new">
           Submit an issue
         </Link>
         <Link href="mailto:david@thecallenders.com">Email me</Link>
